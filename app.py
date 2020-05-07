aecef29f2d32b1935dff4ffcc0e8f09267f4c04d
ariel <arielchoum@gmail.com>
2020-05-06 22:03:38 -0400

    add database

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 #Ariel Chouminov
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///database.db' #set
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False) #Don't let user to leave this empty
+    date_created = db.Column(db.DateTime, default=datetime.utcnow) #will automatically be set to the time
+
+    def __repr__(self):
+        return '<Task %r>' % self.id #every time a task is created it will return the id
 
 #create index route
 @app.route('/')
